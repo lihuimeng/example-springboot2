83d81fc579d748996c9f89d992efcd4786ac78ac
huimeng.li <huimeng.li@mobilemd.cn>
2018-07-03 11:47:25 +0800

    添加服务提供者(provider-movie)整合mybatis

--- provider-movie/src/main/java/com/taimei/example/ProviderMovieApplication.java
+++ provider-movie/src/main/java/com/taimei/example/ProviderMovieApplication.java
@@ -1,11 +1,13 @@
 package com.taimei.example;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan(value = "com.taimei.example.dao")
 public class ProviderMovieApplication {
 
     public static void main(String[] args) {
